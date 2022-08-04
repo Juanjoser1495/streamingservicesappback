9bdb1193124011e71f174d1c1523b5b57214d198
juan.rojas <juanjoserojasv@gmail.com>
2022-08-04 16:16:54 -0500

    First commit

---
+++ streamingapp/src/main/java/com/selfdevelopment/streamingapp/StreamingappApplication.java
@@ -0,0 +1,13 @@
+package com.selfdevelopment.streamingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingappApplication.class, args);
+	}
+
+}
---
+++ streamingapp/src/test/java/com/selfdevelopment/streamingapp/StreamingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfdevelopment.streamingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

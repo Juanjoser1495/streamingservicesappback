3cde49345c3c368728fa8be95899c4142ed5f1f4
juan.rojas <juanjoserojasv@gmail.com>
2022-08-05 11:58:48 -0500

    Adding cross origin to localhost

--- streamingapp/src/main/java/com/selfdevelopment/streamingapp/controller/StreamingAppController.java
+++ streamingapp/src/main/java/com/selfdevelopment/streamingapp/controller/StreamingAppController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,7 @@ public class StreamingAppController {
 		this.rankingService = rankingService;
 	}
 	
+	@CrossOrigin(origins = "http://localhost:3000")
 	@GetMapping("/genre/{category}")
 	public ResponseEntity<RankingTop10Response> getTop10FromGenre(@PathVariable("category") String category){
 		List<RankingTop10Mapped> rankingTop10 = rankingService.fetchTop10ByGenre(category);

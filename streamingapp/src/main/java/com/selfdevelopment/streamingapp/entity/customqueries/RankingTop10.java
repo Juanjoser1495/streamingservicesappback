2a9ac23fc8baf164def6898ae554fe58a197aa81
juan.rojas <juanjoserojasv@gmail.com>
2022-08-12 16:36:49 -0500

    Adding constructor, annotation on service and setters on projection interface

--- streamingapp/src/main/java/com/selfdevelopment/streamingapp/entity/customqueries/RankingTop10.java
+++ streamingapp/src/main/java/com/selfdevelopment/streamingapp/entity/customqueries/RankingTop10.java
@@ -6,4 +6,9 @@ public interface RankingTop10 {
 	String getImageurl();
 	String getGenre();
 	Integer getLikes();
+	
+	String setMoviename(String movieName);
+	String setImageurl(String imageUrl);
+	String setGenre(String genre);
+	Integer setLikes(Integer likes);
 }

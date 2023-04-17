3ef3da052c58e12e22df674cfd2d21af48c53b94
Juan Jose Rojas Velez <juanjoserojasv@gmail.com>
2023-04-17 14:03:11 -0400

    Adding the script for create tables and modifing the name of database

---
+++ streamingapp/Database.sql
@@ -0,0 +1,24 @@
+CREATE TABLE [IF NOT EXISTS] movie (
+   idgenre serial PRIMARY KEY,
+   genre varchar(10000),
+);
+
+CREATE TABLE [IF NOT EXISTS] serie (
+   idserie serial PRIMARY KEY,
+   nameserie varchar(10000),
+   imageurl varchar(10000),
+   genreid serial,
+   likes int,
+   FOREIGN KEY(genreid)
+        REFERENCES genre(idgenre)
+);
+
+CREATE TABLE [IF NOT EXISTS] movie (
+   idmovie serial PRIMARY KEY,
+   moviename varchar(10000),
+   imageurl varchar(10000),
+   genreid serial,
+   likes int,
+   FOREIGN KEY(genreid)
+        REFERENCES genre(idgenre)
+);
